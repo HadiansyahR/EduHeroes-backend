ef6335c4db1688e638191ee7133f23d7d8751f83
HadiansyahR <hadiansyah21rabbani5@gmail.com>
2026-03-11 06:56:48 +0700

    Authentication and login functionality

---
+++ api/v1/auth.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from starlette import status
+
+from database import get_db
+from schemas.auth import LoginRequest, Token
+from services.auth_service import authenticate_user
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+
+# @router.post("/login", response_model=Token, status_code=status.HTTP_200_OK)
+# def login_user(data: LoginRequest, db: Session = Depends(get_db)):
+#     token = authenticate_user(db, data.identifier, data.password)
+#     if not token:
+#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+#
+#     return {
+#         "access_token": token,
+#         "token_type": "bearer"
+#     }
+
+@router.post("/login", response_model=Token, status_code=status.HTTP_200_OK)
+def login_user(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    token = authenticate_user(db, form_data.username, form_data.password)
+    if not token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
--- api/v1/users.py
+++ api/v1/users.py
@@ -5,9 +5,14 @@ from starlette import status
 from database import get_db
 from schemas.user import UserCreate, UserRead
 from services.user_service import create_user
+from core.auth import get_current_user
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=UserRead)
 def register_user(user: UserCreate, db: Session = Depends(get_db)):
     return create_user(db, user)
+
+@router.get("/me", status_code=status.HTTP_200_OK)
+def get_me(current_user = Depends(get_current_user)):
+    return current_user
